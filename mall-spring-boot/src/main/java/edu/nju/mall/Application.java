e9db1d5d5a9308ccb993cd3f7d3589cf59f32388
Hermit <yzy627@126.com>
2020-01-30 23:36:41 +0800

    init

---
+++ mall-spring-boot/src/main/java/edu/nju/mall/Application.java
@@ -0,0 +1,16 @@
+package edu.nju.mall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author hermc
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
