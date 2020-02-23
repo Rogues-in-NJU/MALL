9a90f31bf581b93546cb1270bafb89ea6b6393d1
yqe <541123989@qq.com>
2020-02-23 20:16:11 +0800

    fix

--- mall-angular/src/app/core/services/withdrawl.service.ts
+++ mall-angular/src/app/core/services/withdrawl.service.ts
@@ -5,6 +5,7 @@ import { Observable } from "rxjs";
 import { WithdrawlCondition } from "../model/withdrawlCondition";
 import { WithdrawlRecord } from "../model/withdrawlRecord";
 import { AppConfig } from "../../../environments/environment";
+import {ClassificationVO} from "../model/classification";
 
 @Injectable({
   providedIn: 'root'
@@ -17,6 +18,19 @@ export class WithdrawlService {
 
   }
 
+  public findTodoRecordList(queryParams: TableQueryParams): Observable<ResultVO<TableResultVO<WithdrawlRecord>>> {
+    return this.http.get<ResultVO<TableResultVO<ClassificationVO>>>(`${AppConfig.BASE_URL}/api/withDrawl/todoRecordList`, {
+      params: queryParams,
+      withCredentials: true
+    });
+  };
+
+  public findDoneRecordList(queryParams: TableQueryParams): Observable<ResultVO<TableResultVO<WithdrawlRecord>>> {
+    return this.http.get<ResultVO<TableResultVO<ClassificationVO>>>(`${AppConfig.BASE_URL}/api/withDrawl/doneRecordList`, {
+      params: queryParams,
+      withCredentials: true
+    });
+  };
 
 
 }
