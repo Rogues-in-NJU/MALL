1652232e86b3bea7f2b569d557e037995da1b860
yqe <541123989@qq.com>
2020-03-12 23:48:38 +0800

    fix

--- mall-spring-boot/src/main/java/edu/nju/mall/Application.java
+++ mall-spring-boot/src/main/java/edu/nju/mall/Application.java
@@ -13,6 +13,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 public class Application {
 
     public static void main(String[] args) {
+        System.setProperty("tomcat.util.http.parser.HttpParser.requestTargetAllow","|{}");
         SpringApplication.run(Application.class, args);
     }
 
