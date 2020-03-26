1d3f5feb25a9b88b814ee17d49718e2cb4108bb5
xujiangcheng <mf1832197@smail.nju.edu.cn>
2020-03-26 19:57:45 +0800

    fix  image

--- mall-spring-boot/src/main/java/edu/nju/mall/controller/wechat/WechatProductController.java
+++ mall-spring-boot/src/main/java/edu/nju/mall/controller/wechat/WechatProductController.java
@@ -11,6 +11,9 @@ import edu.nju.mall.vo.ProductVO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -33,8 +36,8 @@ public class WechatProductController {
     @GetMapping("/product/list")
     public ResultVO<ListResponse> getProductList(@RequestParam(value = "pageIndex", defaultValue = "1") final Integer pageIndex,
                                                @RequestParam(value = "pageSize", defaultValue = "10") final Integer pageSize) {
-        Page<ProductVO> products = productService.getProductPage(pageIndex,pageSize);
-        return ResultVO.ok(ListResponseUtils.generateResponse(products, pageIndex, pageSize));
+        Pageable pageable = PageRequest.of(pageIndex - 1, pageSize, Sort.by(Sort.Direction.ASC, "createdAt"));
+        return ResultVO.ok(ListResponseUtils.generateResponse(productService.getProductList(pageable), pageIndex, pageSize));
     }
 
     @InvokeControl
